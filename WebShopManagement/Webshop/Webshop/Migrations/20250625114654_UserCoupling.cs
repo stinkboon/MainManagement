abc49a404fbf07dfec1b40084bc260d5af65bcea
Dani Rimmelzwaan <DaniRimmelzwaan@gmail.com>
2025-06-25 14:59:10 +0200

    Usercoupling

---
+++ WebShopManagement/Webshop/Webshop/Migrations/20250625114654_UserCoupling.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Webshop.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserCoupling : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "DiscountPercentage",
+                table: "Products",
+                type: "numeric",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Products",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Customers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_UserId",
+                table: "Products",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_UserId",
+                table: "Customers",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Customers_Users_UserId",
+                table: "Customers",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Users_UserId",
+                table: "Products",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Customers_Users_UserId",
+                table: "Customers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Users_UserId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_UserId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Customers_UserId",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Customers");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DiscountPercentage",
+                table: "Products",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "numeric",
+                oldNullable: true);
+        }
+    }
+}
