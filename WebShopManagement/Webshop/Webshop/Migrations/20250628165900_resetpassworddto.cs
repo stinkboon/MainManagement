1809f33e8a99ae00aad39ee965850e1b665ec578
Dani Rimmelzwaan <DaniRimmelzwaan@gmail.com>
2025-06-28 19:00:43 +0200

    commit passwordresetdto

---
+++ WebShopManagement/Webshop/Webshop/Migrations/20250628165900_resetpassworddto.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Webshop.Migrations
+{
+    /// <inheritdoc />
+    public partial class resetpassworddto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ResetToken",
+                table: "Users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ResetTokenExpires",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ResetToken",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ResetTokenExpires",
+                table: "Users");
+        }
+    }
+}
